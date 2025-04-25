c0b8fac13c656288b38272cb3621a0bd5c9cf52e
Pepeshka32 <iwashkalolesnikow@gmail.com>
2025-04-25 09:14:04 +0300

    zz

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './components/header.js';
 import { AboutUs } from './components/abous us.js';
 import { Conection } from './components/connection.js';
+import { Main } from './components/main.js';
 import { Prefooter } from './components/prefooter.js';
 import { Footer } from './components/footer.js';
 
@@ -10,16 +11,7 @@ import { Footer } from './components/footer.js';
 function App() {
   return (
     <BrowserRouter>
-      <Header >
-      </Header>
-      <AboutUs>
-      </AboutUs>
-      <Prefooter>
-      </Prefooter>
-      <Footer>
-      </Footer>
-      <Routes>
-      </Routes>
+<Main/>
     </BrowserRouter>
 
 
